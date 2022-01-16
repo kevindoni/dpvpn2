5f23c0895bac306d0e016ce7a35ee2910da1f29f
kevindoni <kevindoni17@gmail.com>
2022-01-16 09:44:25 +0700

    Update update.sh

--- kepo/update.sh
+++ kepo/update.sh
@@ -6,7 +6,7 @@ NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo -e  "${RED}Checking VPS${NC}"
 sleep 2
-IZIN=$( curl https://raw.githubusercontent.com/dpvpn09/ipvps/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/kevindoni/dpvpn2/main/kepo/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${GREEN}Permission Accepted...${NC}"
 sleep 2 
